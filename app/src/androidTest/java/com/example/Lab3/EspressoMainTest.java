8ab7e84ed2c8db90fdc177c441a5fd441464a1cd
avk256 <avk256@gmail.com>
2022-10-18 21:08:14 +0300

    Add espresso test

---
+++ app/src/androidTest/java/com/example/Lab3/EspressoMainTest.java
@@ -0,0 +1,42 @@
+package com.example.Lab3;
+
+import android.app.Activity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.filters.SmallTest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class EspressoMainTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withId(R.id.time)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void listGoesOverTheFold2() {
+        onView(withId(R.id.time)).check(matches(isDisplayed()));
+    }
+
+}
